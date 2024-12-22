9e9e66c9e9644d7ca20fddb0f6300ea1467272e3
abohebil <mahmoudabohebil@gmail.com>
2024-12-22 23:13:30 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/abohebil/fresh_feed/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.abohebil.fresh_feed
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/app/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:fresh_feed/screens/screens.dart';
+
+class FreshFeedApp extends StatelessWidget {
+  const FreshFeedApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}

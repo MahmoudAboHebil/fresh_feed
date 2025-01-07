eef815feb1f316a8f913aaada09b16387a4d7b94
abohebil <mahmoudabohebil@gmail.com>
2025-01-07 18:48:32 +0200

    refactoring and thinking about the next steps

--- lib/main.dart
+++ lib/main.dart
@@ -5,17 +5,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'app/app.dart';
 import 'firebase_options.dart';
 
-//enable user to verified its email with error handling /done
-//enable user to reset its password with error handling /done
-///ToDO: enable user sign in with phone with error handling
-
-///(important because currently i update
-/// the profile whenever user logged in and signed up, so take care) => (done now
-///  it is  save)
-///ToDO: enable user profile within firestore and firebase
-///ToDO: handling the authentication process if network connection is lost
-///TODO: you need to handle if user is unlogged to push to signIn page
-///and you need care about guest-app feature
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
